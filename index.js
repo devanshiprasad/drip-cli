67f1c947d9a8ecc716e285164bbd04533564701b
Devanshi Prasad <devanshiprasad@outlook.com>
2024-10-09 19:09:21 +0530

    removed docker file

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ require('dotenv').config();
 
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
+
 const client = twilio(accountSid, authToken);
 
 const rl = readline.createInterface({
@@ -16,7 +17,7 @@ const rl = readline.createInterface({
 rl.question('Enter the reminder interval in hours: ', (interval) => {
   rl.question('Enter the start time (24hr format, e.g., 08 for 8 AM): ', (startTime) => {
     rl.question('Enter the end time (24hr format, e.g., 20 for 8 PM): ', (endTime) => {
-      const cronExpression = `0 ${startTime}-${endTime}/${interval} * * *`;
+      const cronExpression = `* ${startTime}-${endTime} * * *`;
       
       cron.schedule(cronExpression, () => {
         console.log('Reminder: Take a break and drink water!');
