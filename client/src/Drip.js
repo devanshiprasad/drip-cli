7c5dfc8b957c5580dd0e4f4f1a69062b460068fb
Devanshi Prasad <devanshiprasad@outlook.com>
2024-10-10 11:01:54 +0530

    added welcome page, main page and css

--- client/src/Drip.js
+++ client/src/Drip.js
@@ -1,6 +1,7 @@
 // client/src/App.js
 import React, { useState } from 'react';
 import axios from 'axios';
+import './App.css';
 
 function Drip() {
     const [interval, setInterval] = useState('');
@@ -25,40 +26,60 @@ function Drip() {
     };
 
     return (
-        <div>
-            <h1>Water Reminder App</h1>
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="number"
-                    placeholder="Interval (in minutes)"
-                    value={interval}
-                    onChange={(e) => setInterval(e.target.value)}
-                    required
-                />
-                <input
-                    type="number"
-                    placeholder="Start Time (24hr)"
-                    value={startTime}
-                    onChange={(e) => setStartTime(e.target.value)}
-                    required
-                />
-                <input
-                    type="number"
-                    placeholder="End Time (24hr)"
-                    value={endTime}
-                    onChange={(e) => setEndTime(e.target.value)}
-                    required
-                />
-                <input
-                    type="text"
-                    placeholder="Your WhatsApp Number"
-                    value={phoneNumber}
-                    onChange={(e) => setPhoneNumber(e.target.value)}
-                    required
-                />
-                <button type="submit">Set Reminder</button>
-            </form>
-        </div>
+        <div className="form-container">
+        <h1 id='drip'>Water Reminder App</h1>
+        <form onSubmit={handleSubmit} className="flex">
+            <div className='flex-items'>
+                <label>
+                    Interval (in minutes):
+                    <input
+                        type="number"
+                        placeholder="Interval (in minutes)"
+                        value={interval}
+                        onChange={(e) => setInterval(e.target.value)}
+                        required
+                    />
+                </label>
+            </div>
+            <div className='flex-items'>
+                <label>
+                    Start Time (24hr):
+                    <input
+                        type="number"
+                        placeholder="Start Time (24hr)"
+                        value={startTime}
+                        onChange={(e) => setStartTime(e.target.value)}
+                        required
+                    />
+                </label>
+            </div>
+            <div className='flex-items'>
+                <label>
+                    End Time (24hr):
+                    <input
+                        type="number"
+                        placeholder="End Time (24hr)"
+                        value={endTime}
+                        onChange={(e) => setEndTime(e.target.value)}
+                        required
+                    />
+                </label>
+            </div>
+            <div className='flex-items'>
+                <label>
+                    Your WhatsApp Number (prefix your country code without a "+" or space):
+                    <input
+                        type="text"
+                        placeholder="Your WhatsApp Number"
+                        value={phoneNumber}
+                        onChange={(e) => setPhoneNumber(e.target.value)}
+                        required
+                    />
+                </label>
+            </div>
+            <button type="submit">Set Reminder</button>
+        </form>
+    </div>
     );
 }
 
--- client/src/Home.js
+++ client/src/Home.js
@@ -1,7 +1,21 @@
 import React from 'react'
-
+import logo from './images/logo.png'
+import qr from './images/qr.png'
+import { Link } from 'react-router-dom';
+import './App.css';
 export default function Home() {
   return (
-    <div>Hey!</div>
+    <div className="flex-container">
+   <div className="flex-items" ><img src={logo} id='logo'/></div>
+   <div className="flex-items" id="para"><p id='text'>Welcome to DRIP: an app that reminds you stay hydrated- <br/>
+     by sending you reminders through <b>WhatsApp</b>.</p></div>
+   <div className='flex-items'><img src={qr} id='qr'/></div>
+   <div className='flex-items' id='qr-text'><p id='pqr-text'>Scan this QR code to add your whatsapp number to my Twilio sandbox!</p></div>
+   <Link to='/Drip'>
+   <button class="bn54">
+    <span class="bn54span">let's go!</span>
+  </button>
+   </Link>
+</div>
   )
 }
