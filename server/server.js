6dbe4472ee290b6484bc07cd3400220486b937fe
Devanshi Prasad <devanshiprasad@outlook.com>
2024-10-12 21:41:12 +0530

    changed ip addresses

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = twilio(accountSid, authToken);
 
 // Middleware
-app.use(cors()); 
+app.use(cors(({ origin: 'http://3.110.37.144:3000' }))); 
 app.use(bodyParser.json()); 
 
 // Rate Limiter Middleware
@@ -46,5 +46,5 @@ app.post('/api/reminder', limiter, (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on http://52.66.242.39:${port}`);
+    console.log(`Server is running on http://3.110.37.144:${port}`);
 });
